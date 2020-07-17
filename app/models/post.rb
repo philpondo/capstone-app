8eced0a456e5df9d07d1787dd483f7870f533f55
phil <phil.pondo@gmail.com>
2020-07-17 15:11:44 -0400

    adds validations across models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :conversations, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :players_needed, presence: true
+  validates :players_needed, inclusion: { :in => [1, 2, 3, 4] }
 end
