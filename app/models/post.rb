21ccde77dcca24e887f142a97a8ebed7f39778d7
phil <phil.pondo@gmail.com>
2020-07-24 12:31:35 -0400

    adds length validation for post title and content

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :conversations, dependent: :destroy
 
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 500 }
   validates :players_needed, presence: true
   validates :players_needed, inclusion: { :in => [1, 2, 3, 4] }
 end
